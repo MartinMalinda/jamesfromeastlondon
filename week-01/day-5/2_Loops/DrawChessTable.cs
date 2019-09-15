d6ede4a104c99581200d89de8b79d291ce199f95
James Dang <jamesfromeastlondon@gmail.com>
2019-09-15 11:42:52 +0200

    completed draw chessboard exercise

---
+++ week-01/day-5/2_Loops/DrawChessTable.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that draws a chess table like this
+            //
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            //
+
+            for (int row = 0; row < 8; row ++)
+            {
+                if (row % 2 == 0)
+                {
+                    Console.WriteLine("% % % %");
+
+                }
+                else
+                {
+                    Console.WriteLine(" % % % %");
+                }
+            }
+        }
+    }
+}
