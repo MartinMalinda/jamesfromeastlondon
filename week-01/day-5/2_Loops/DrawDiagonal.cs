f54ee14c03acc180bb99a55cd6ccc3d3cedc91d9
James Dang <jamesfromeastlondon@gmail.com>
2019-09-15 10:55:36 +0200

    improved legibility of draw diagonal exercise

--- week-01/day-5/2_Loops/DrawDiagonal.cs
+++ week-01/day-5/2_Loops/DrawDiagonal.cs
@@ -17,23 +17,23 @@ namespace GreenFox
             // %%%%%
             //
             // The square should have as many lines as the number was
+
             Console.WriteLine("How many lines tall will your square (with diagonal) be?");
             int userNumber = Convert.ToInt32(Console.ReadLine());
+           
+            int row, column;
+            for (row = 1; row <= userNumber; row ++)
             {
-                int row, column;
-                for (row = 1; row <= userNumber; row ++)
+                for (column = 1; column <= userNumber; column ++)
                 {
-                    for (column = 1; column <= userNumber; column++)
-                    {
-                        if (row == 1 || row == userNumber || // top row, bottom row
-                            column == 1 || column == userNumber || // start column, end column
-                            row == column) // diagonal
-                            Console.Write("*");
-                        else
-                            Console.Write(" ");
-                    }
-                    Console.WriteLine();
+                    if (row == 1 || row == userNumber || // top row, bottom row
+                        column == 1 || column == userNumber || // start column, end column
+                        row == column) // diagonal
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
                 }
+                Console.WriteLine();
             }
         }
     }
