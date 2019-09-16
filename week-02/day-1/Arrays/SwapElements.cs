c6f6bf2e11854da640c72f209f96eb5ee6b28a2d
James Dang <jamesfromeastlondon@gmail.com>
2019-09-16 16:24:18 +0200

    completed swap elements task

---
+++ week-02/day-1/Arrays/SwapElements.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `abc`
+            //   with the following content: `["first", "second", "third"]`
+            // - Swap the first and the third element of `abc`
+
+            string[] abc = { "first", "second", "third" };
+
+            string temp = abc[0];
+            abc[0] = abc[2];
+            abc[2] = temp;
+
+            foreach (var word in abc)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
