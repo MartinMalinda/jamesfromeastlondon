725068f6e3691b3f6ba1fc1a458151752b0bfee3
James Dang <jamesfromeastlondon@gmail.com>
2019-09-16 15:17:00 +0200

    completed double items task

---
+++ week-02/day-1/Arrays/DoubleItems.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `numList`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Double all the values in the array
+
+            int[] numList = { 3, 4, 5, 6, 7 };
+
+            for (int row = 0; row < numList.GetLength(0); row++)
+            {
+                Console.WriteLine(numList[row]*2);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
