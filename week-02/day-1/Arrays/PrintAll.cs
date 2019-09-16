242dffaee1215e98a13e5f2dd572b8d5e09c13ce
James Dang <jamesfromeastlondon@gmail.com>
2019-09-16 13:42:18 +0200

    completed print all task

---
+++ week-02/day-1/Arrays/PrintAll.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace PrintAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `numbers`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print all the elements of `numbers`
+
+            int[] numbers = { 4, 5, 6, 7 };
+
+            foreach (int number in numbers)
+            {
+            Console.WriteLine(number);                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
