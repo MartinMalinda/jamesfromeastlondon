eef8c5158b3039bd6c360cab6b486b630d38297f
James Dang <jamesfromeastlondon@gmail.com>
2019-09-17 22:35:27 +0200

    completed map introduction 2 task

--- week-02/day-2/Data structures/Map introduction 2.cs
+++ week-02/day-2/Data structures/Map introduction 2.cs
@@ -15,11 +15,44 @@ namespace Map
             diction.Add("978-1-60309-444-3", "Red Panda and Moon Bear");
             diction.Add("978-1-60309-461-0", "The Lab");
 
+            foreach (var pair in diction)
+            {
+                Console.WriteLine("{0} (ISBN: {1})", pair.Value, pair.Key);
+            }
+
+            // Remove the key-value pair with key 978-1-60309-444-3
+            diction.Remove("978-1-60309-444-3");
+
+            // Remove the key-value pair with value The Lab
+            foreach (var item in diction.Where(kvp => kvp.Value == "The Lab").ToList())
+            {
+                diction.Remove(item.Key);
+            }
+
+            Console.WriteLine("\nUpdated list with items removed:");
 
             foreach (var pair in diction)
             {
                 Console.WriteLine("{0} (ISBN: {1})", pair.Value, pair.Key);
             }
+
+            // Added 2 key-value pairs
+            diction.Add("978-1-60309-450-4", "They Called Us Enemy");
+            diction.Add("978-1-60309-453-5", "Why Did We Trust Him?");
+            
+            // Check for a key-value pair
+            string getValue = "";
+            if (diction.TryGetValue("478-0-61159-424-8", out getValue))
+            {
+                Console.WriteLine("\nKey: 478-0-61159-424-8 contains {0}", getValue);
+            }
+            else
+            {
+                Console.WriteLine("\nKey: 478-0-61159-424-8 cannot be found.");
+            }
+
+            // Print an key-value pair
+            Console.WriteLine(diction["978-1-60309-453-5"]);
         }
     }
 }
