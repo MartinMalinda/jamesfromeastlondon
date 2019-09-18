c767d2af146945f09a064ca0d41ed6aae447391a
James Dang <jamesfromeastlondon@gmail.com>
2019-09-18 17:29:59 +0200

    completed purple steps 3d

---
+++ week-02/day-3/12_Purple steps 3d/DrawingApplication/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using GreenFox;
+
+namespace DrawingApplication
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+            // Reproduce this:
+            // [https://github.com/green-fox-academy/chama-cs-prg-syllabus/blob/master/workshop/drawing/assets/r4.png]
+
+            DrawLine(foxDraw);
+        }
+
+        public static void DrawLine(FoxDraw foxDraw)
+        {
+            foxDraw.SetFillColor(Colors.Purple);
+            foxDraw.SetStrokeThicknes(1);
+            int square = 4;
+            for (int shape = 0; shape < 6; shape++)
+            {
+                square += square;
+                foxDraw.DrawRectangle(square, square, square, square);            
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
