009ab68a3666868fd3c9ac25db53d056848e6287
James Dang <jamesfromeastlondon@gmail.com>
2019-09-18 15:48:50 +0200

    completed drawing diangonals task

---
+++ week-02/day-3/3_Diagonals/DrawingApplication/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using GreenFox;
+
+namespace DrawingApplication
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+            // Draw the canvas' diagonals.
+            // If it starts from the upper-left corner it should be green, otherwise it should be red.
+
+            canvas.Width = 500;
+            canvas.Height = 500;
+            foxDraw.SetBackgroundColor(Colors.Yellow);
+
+            DrawLine(foxDraw);
+        }
+
+        public static void DrawLine(FoxDraw foxDraw)
+        {
+            foxDraw.SetStrokeThicknes(2);
+            var startPoint = new Point(0, 0);
+            var endPoint = new Point(500, 500);
+
+            if (startPoint == new Point(0, 0))
+            {
+                foxDraw.SetStrokeColor(Colors.Green);
+            }
+            else
+            {
+                foxDraw.SetStrokeColor(Colors.Red);
+            }
+            foxDraw.DrawLine(startPoint, endPoint);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
