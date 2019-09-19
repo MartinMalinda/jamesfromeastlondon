252db3468fd6892ec4650a1297582402209c88b2
James Dang <jamesfromeastlondon@gmail.com>
2019-09-19 10:23:11 +0200

    completed drawing tasks 5 and 8

---
+++ week-02/day-4/5_Horizontal/DrawingApplication/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using GreenFox;
+
+namespace DrawingApplication
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+            // Create a function that draws a single line and takes 3 parameters:
+            // The x and y coordinates of the line's starting point and the foxDraw
+            // and draws a 50 long horizontal line from that point.
+            // Draw at least 3 lines with that function using a loop.
+
+            canvas.Width = 200;
+            canvas.Height = 200;
+            foxDraw.SetBackgroundColor(Colors.Yellow);
+
+            for (int i = 0; i < 3; i++)
+            {
+                DrawLine(foxDraw, 10, i * 50 + 10 ); // Set x + y values for DrawLine method, y changes for every loop
+            }
+        }
+
+        public static void DrawLine(FoxDraw foxDraw, int x, int y)
+        {
+            var startPoint = new Point(x, y); 
+            var endPoint = new Point(x + 50, y); // End point makes the line 50 px long
+            foxDraw.SetStrokeThicknes(2);
+            foxDraw.SetStrokeColor(Colors.Green);
+            foxDraw.DrawLine(startPoint, endPoint);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ week-02/day-4/8_Square positioning/DrawingApplication/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using GreenFox;
+
+namespace DrawingApplication
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+            // create a function that draws one square and takes 3 parameters:
+            // the x and y coordinates of the square's top left corner
+            // and the foxDraw and draws a 50x50 square from that point.
+            // draw 3 squares with that function.
+            // avoid code duplication.
+
+            for (int i = 0; i < 3; i++)
+            {
+                DrawSquare(foxDraw, i * 150 + 50, 50);
+            }
+        }
+
+        public static void DrawSquare(FoxDraw foxDraw, int x, int y)
+        {
+            foxDraw.SetFillColor(Colors.Red);
+            foxDraw.DrawRectangle(x, y, 50, 50);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
