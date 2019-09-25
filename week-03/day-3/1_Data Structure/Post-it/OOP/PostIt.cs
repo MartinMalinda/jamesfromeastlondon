671a7d5f4bed532b9eb9b1c7c609c41e66a30030
James Dang <jamesfromeastlondon@gmail.com>
2019-09-25 11:44:44 +0200

    completed data structure post-it task

---
+++ week-03/day-3/1_Data Structure/Post-it/OOP/PostIt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace OOP
+{
+    public class PostIt
+    {
+        public string backgroundColor { get; set; }
+        public string text { get; set; }
+        public string textColor { get; set; }
+
+        public PostIt()
+        {
+
+        }
+
+        public PostIt(string backgroundColor, string text, string textColor)
+        {
+            this.backgroundColor = backgroundColor;
+            this.text = text;
+            this.textColor = textColor;
+        }
+    }
+}
---
+++ week-03/day-3/1_Data Structure/Post-it/OOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostIt postItOne = new PostIt();
+            {
+                postItOne.backgroundColor = "Orange";
+                postItOne.text = "Idea 1";
+                postItOne.textColor = "Blue";
+            }
+            PostIt postItTwo = new PostIt();
+            {
+                postItTwo.backgroundColor = "Pink";
+                postItTwo.text = "Awesome";
+                postItTwo.textColor = "Black";
+            }
+            PostIt postItThree = new PostIt();
+            {
+                postItThree.backgroundColor = "Yellow";
+                postItThree.text = "Superb!";
+                postItThree.textColor = "Green";
+            }
+        }   
+    }
+}
