114c534c5c6c93d2e6e17ef69d1f9ce0eab88b94
James Dang <jamesfromeastlondon@gmail.com>
2019-10-02 18:45:19 +0200

    completed first part of comparable fleet of things task: sorting by completed status

---
+++ week-04/day-3/interface/comparable_fleet_of_things/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static comparable.Thing;
+
+namespace comparable
+{
+    public class FleetOfThings
+    {
+        public static void Main(string[] args)
+        {
+            var fleet = new Fleet();
+            //var obj = new Thing(CompareTo());
+            //obj.Sort();
+            // - You have the `Thing` class
+            // - You have the `Fleet` class
+            // - You have the `FleetOfThings` class with a `Main` method
+            // - Download those, use those
+            // - In the Main method create a fleet
+            // - Achieve this output:s
+            // Create a fleet of things to have this output:
+            // 1. [ ] Get milk
+            // 2. [ ] Remove the obstacles
+            // 3. [x] Stand up
+            // 4. [x] Eat lunch
+            // Hint: You have to create a Print method also
+
+            fleet.Add(new Thing("Get milk"), false);
+            fleet.Add(new Thing("Remove the obstacles"), false);
+            fleet.Add(new Thing("Stand up"), true);
+            fleet.Add(new Thing("Eat lunch"), true);
+
+            fleet.Sort();
+            fleet.Reverse();
+            fleet.Print();
+        }
+    }
+}
+
+//Make your Todo class implement Comparable
+//It should compare the completed field first
+//Then the description
---
+++ week-04/day-3/interface/comparable_fleet_of_things/Things.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace comparable
+{
+    public class Thing : IComparable<Thing>
+    {
+        public string name;
+        public bool completed;
+
+        public Thing(string name)
+        {
+            this.name = name;
+        }
+
+        public void Complete()
+        {
+            completed = true;
+        }
+
+        public virtual int CompareTo(Thing other)
+        {
+            return completed.CompareTo(other.completed);
+        }
+    }
+}
