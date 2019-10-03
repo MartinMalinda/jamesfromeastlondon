1a44c6de488bb4ad3653a0b9eee32a36b28c1d2f
James Dang <jamesfromeastlondon@gmail.com>
2019-10-03 17:19:06 +0200

    completed fibonacci testing task

---
+++ week-04/day-4/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    public class GenerateFibonacci
+    {
+        public int GenFibonacci(int num)
+        {
+            if (num <= 1)
+            {
+                return num;
+            }
+            return GenFibonacci(num - 1) + GenFibonacci(num - 2);
+        }
+    }
+    class Program
+    {
+        public static int GenFibonacci(int num)
+        {
+            if (num <= 1)
+            {
+                return num;
+            }
+            return GenFibonacci(num - 1) + GenFibonacci(num - 2);
+        }
+        static void Main(string[] args)
+        {
+            int num = 5;
+            Console.WriteLine(GenFibonacci(num)); 
+        }
+    }
+}
+
+//Write a function that computes a member of the fibonacci sequence by a given index
+//Create tests for multiple test cases.
---
+++ week-04/day-4/Fibonacci/Testing/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+using Fibonacci;
+
+namespace Testing
+{
+    public class UnitTest1
+    {
+        private GenerateFibonacci generateFibonacci;
+        public UnitTest1()
+        {
+            generateFibonacci = new GenerateFibonacci();
+        }
+
+        [Fact]
+        public void TestIndexOne()
+        {
+            int inputOne = 1;
+            int inputFive = 5;
+            int inputTen = 10;
+
+            Assert.Equal(1, generateFibonacci.GenFibonacci(inputOne));
+            Assert.Equal(5, generateFibonacci.GenFibonacci(inputFive));
+            Assert.Equal(55, generateFibonacci.GenFibonacci(inputTen));
+        }
+    }
+}
