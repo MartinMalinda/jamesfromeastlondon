32166bf72ffd25c47799dda8416d806773047277
James Dang <jamesfromeastlondon@gmail.com>
2019-10-03 11:38:43 +0200

    completed apples testing task

---
+++ week-04/day-4/Apples/Testing/UnitTest1.cs
@@ -0,0 +1,34 @@
+using System;
+using Xunit;
+using Apples;
+
+namespace Testing
+{
+    public class UnitTest1
+    {
+        private Applez applez;
+        public UnitTest1()
+        {
+            applez = new Applez();
+        }
+
+        [Fact]
+        public void TestString()
+        {
+            Assert.Equal("apple", applez.GetApple());
+        }
+    }
+}
+
+//Create a class, with one method(eg. public string GetApple()) that returns a string (eg. "apple")
+//Create a test for that.
+//Create an xUnit project
+//Add a new Test case in it
+//Instantiate your class (arrange, act, assert)
+//Use the Assert.Equal()
+//The expected parameter should be the same string (eg. "apple")
+//The actual parameter should be the return value of the method(eg.myObject.GetApple())
+//Run the test
+//Change the expected value to make the test failing
+//Run the test
+//Fix the returned value to make the test succeeding again
