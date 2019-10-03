88cd8802226b1cfc479cfcccfdf994a7919750e2
James Dang <jamesfromeastlondon@gmail.com>
2019-10-03 14:45:34 +0200

    completed anagram testing task

---
+++ week-04/day-4/Anagram/Anagram/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Anagram
+{
+    public class Anagrammy
+    {
+        public bool AnagramChecker(string strA, string strB)
+        {
+            if (strA.Length != strB.Length)
+            {
+                return false;
+            }
+
+            char[] sortA = strA.ToCharArray();
+            char[] sortB = strB.ToCharArray();
+
+            Array.Sort(sortA);
+            Array.Sort(sortB);
+
+            for (int i = 0; i < strA.Length; i++)
+            {
+                if (sortA[i] != sortB[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+//Write a function, that takes two strings and returns a boolean value
+//based on if the two strings are Anagramms or not.
+//Create a test for that.
---
+++ week-04/day-4/Anagram/Testing/UnitTest1.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+using Anagram;
+
+namespace Testing
+{
+    public class UnitTest1
+    {
+        private Anagrammy anagrammy;
+        public UnitTest1()
+        {
+            anagrammy = new Anagrammy();
+        }
+
+        [Fact]
+        public void TestAnagramChecker()
+        {
+            string strA = "hello";
+            string strB = "olleh";
+
+            bool result = anagrammy.AnagramChecker(strA, strB);
+
+            Assert.True(result);
+        }
+    }
+}
+
+//Write a function, that takes two strings and returns a boolean value
+//based on if the two strings are Anagramms or not.
+//Create a test for that.
