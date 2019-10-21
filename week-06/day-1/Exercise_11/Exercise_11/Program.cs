2c8bdb591eeb2a1fb20dcb729cfa09675d6d1c7d
James Dang <jamesfromeastlondon@gmail.com>
2019-10-21 14:40:55 +0200

    completed - LINQ Exercise 11 - W6 - D1

---
+++ week-06/day-1/Exercise_11/Exercise_11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Exercise_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a single LINQ expression which reads all text from this file,
+            //and prints the 100 most common words in descending order.
+            //Keep in mind that the text contains punctuation characters which should be ignored.
+            var file = File.ReadAllText(@"./wiki_roulette.txt");
+            char[] separators = { ' ', '\n', '\r', ',', '.', '(', ')', '[', ']' };
+            var splitFile = file.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            var fileWordCount = splitFile.GroupBy(f => f).ToList();
+            fileWordCount.ForEach(word => Console.WriteLine(word.Key + ": " + word.Count()));
+        }
+    }
+}
