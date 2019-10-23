33fd89a1e580591438c51fb068ba1d79a37b3da9
James Dang <jamesfromeastlondon@gmail.com>
2019-10-23 17:19:50 +0200

    added ShopItem class - Web Shop - W6 - D4

---
+++ week-06/day-4/WebShop/WebShop/Models/ShopItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace WebShop.Models
+{
+    public class ShopItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int QuanitityOfStock { get; set; }
+
+        public ShopItem()
+        {
+        }
+    }
+}
