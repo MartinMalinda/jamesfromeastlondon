418f2ffd58b7d7a0a98e13c33e76648c99ae9b9c
James Dang <jamesfromeastlondon@gmail.com>
2019-10-24 22:30:20 +0200

    connected MV and C - WebShop - W6 - D4

--- week-06/day-4/WebShop/WebShop/Controllers/ShopController.cs
+++ week-06/day-4/WebShop/WebShop/Controllers/ShopController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebShop.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,18 @@ namespace WebShop.Controllers
 {
     public class ShopController : Controller
     {
+        static List<ShopItem> ShopItemList = new List<ShopItem>() {
+            new ShopItem("Nike Air Sandals", "Enhanced with built-in socks", 1000, 5),
+            new ShopItem("Poppy Seed Juice", "300ml carton (bamboo straw included)", 30, 20),
+            new ShopItem("Bento Box", "A bit of rice and sashimi", 150, 1),
+            new ShopItem("Vintage Pencil", "Graphite pencil (bite marks included)", 10, 5),
+            new ShopItem("Paper plane", "Engineered from cross-laminated toilet paper", 40, 0)
+        };
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            return View(ShopItemList);
         }
     }
 }
--- week-06/day-4/WebShop/WebShop/Models/ShopItem.cs
+++ week-06/day-4/WebShop/WebShop/Models/ShopItem.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace WebShop.Models
 {
     public class ShopItem
@@ -6,7 +8,15 @@ namespace WebShop.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public int Price { get; set; }
-        public int QuanitityOfStock { get; set; }
+        public int QuantityOfStock { get; set; }
+
+        public ShopItem(string name, string description, int price, int quantityOfStock )
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            QuantityOfStock = quantityOfStock;
+        }
 
         public ShopItem()
         {
