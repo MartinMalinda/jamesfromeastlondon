a438630b33df0a9980cd175b5c1520a695967470
James Dang <jamesfromeastlondon@gmail.com>
2019-10-29 10:28:25 +0100

    formatted the balance - Bank of Simba - W7 - D2

---
+++ week-07/day-2/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BankOfSimba.Models;
+
+namespace BankOfSimba.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<BankAccount> accountDetails = new List<BankAccount>()
+        {
+            new BankAccount ( "Simba", 50.00m, "Lion" )
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            //ViewData["Message"] = "Your application description page.";
+            return View(accountDetails);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
