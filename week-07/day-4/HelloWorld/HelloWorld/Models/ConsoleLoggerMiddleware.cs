6c891847f909cc35c5dd7f97b32c7dc8f3999e1e
James Dang <jamesfromeastlondon@gmail.com>
2019-10-31 10:56:42 +0100

    completed exercise 1 - Hello World DI - W7 - D4

---
+++ week-07/day-4/HelloWorld/HelloWorld/Models/ConsoleLoggerMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace HelloWorld.Models
+{
+    public class ConsoleLoggerMiddleware : IMiddleware
+    {
+        private Printer printer;
+
+        public ConsoleLoggerMiddleware(Printer printer)
+        {
+            this.printer = printer;
+        }
+
+        public Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            Console.WriteLine("Hi from the middleware");
+            printer.Log("hello");
+            return next(context);
+        }
+    }
+}
