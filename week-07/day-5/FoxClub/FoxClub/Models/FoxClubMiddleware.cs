c64eac01bc194fefe0546822887da15c42337a08
James Dang <jamesfromeastlondon@gmail.com>
2019-11-01 10:52:08 +0100

    completed introduction - FoxClub - W7 - D5

---
+++ week-07/day-5/FoxClub/FoxClub/Models/FoxClubMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace FoxClub.Models
+{
+    public class FoxClubMiddleware : IMiddleware
+    {
+        public FoxClubMiddleware()
+        {
+        }
+
+        Task IMiddleware.InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            return next (context);
+        }
+    }
+}
